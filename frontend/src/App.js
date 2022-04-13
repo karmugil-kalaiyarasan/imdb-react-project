66edb172cd34e36f7cfc4e6d70142daa765d6f83
karmugil-kalaiyarasan <63269889+karmugil-kalaiyarasan@users.noreply.github.com>
2022-04-12 23:41:34 -0400

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,106 +14,67 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 function App() {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [check, setCheck] = useState(null);
-  const [movieCast, setMovieCast] = useState(null);
-  const [moviesList, setMoviesList] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]); //state for getting the whole popular movie array
+  // const [check, setCheck] = useState(null);
+  // const [movieCast, setMovieCast] = useState(null);
+  const [moviesList, setMoviesList] = useState([]); //state for getting the movie array from the popular movie array
 
   useEffect(() => {
     let done = false;
     const movieFunction = async () => {
-      // axios.get(`http://localhost:8080/`).then((response) => {
-      //   //   console.log(response.data);
-      //   setPopularMovies(response.data);
-      //   console.log(popularMovies);
-      //   // console.log(state);
-      //   // console.log("success");
-      // });
-
       if (done) {
         return;
       }
       try {
         const res = await axios.get(`http://localhost:8080/`);
-        setPopularMovies([res.data]);
-        setMoviesList(popularMovies[0].shows);
+        setPopularMovies([res.data]); //setting up the whole popular movie array
+        setMoviesList(popularMovies[0].shows); //setting up the movie array from the popular movie array
         return () => {
           done = true; //we've finished loading
         };
-        // popularMovies = keyIndex(popularMovies, 1);
-        // setCheck(res.data.shows[1].ids.imdb);
-        // setCheck("tt0848228");
       } catch (err) {}
     };
     movieFunction();
-
-    // const interval = setInterval(() => {
-    //   movieFunction();
-    // }, 10000);
-
-    // return;
   }, [setPopularMovies, setMoviesList]);
 
-  // useEffect(() => {
-  //   const castFunction = async () => {
-  //     // axios.get(`http://localhost:8080/`).then((response) => {
-  //     //   //   console.log(response.data);
-  //     //   setPopularMovies(response.data);
-  //     //   console.log(popularMovies);
-  //     //   // console.log(state);
-  //     //   // console.log("success");
-  //     // });
-
-  //     try {
-  //       const res = await axios.get(`http://localhost:8080/cast/${check}`);
-  //       setMovieCast(res.data);
-  //     } catch (err) {}
-  //   };
-  //   castFunction();
-  // }, [check]);
-
-  console.log(check);
-  // console.log(popularMovies.title);
-  // console.log(movieCast);
-  // console.log(moviesList);
+  console.log(moviesList); //can use this console log to make changes and save to see the issue we talked on the meeting
 
   return (
     <Router>
       <div className="container">
         <Switch>
           <Route exact path="/">
-            {/*{popularMovies.map(
-        (item, index) => (
-          // <Movies item={item} index={index} key={index} />
-          <Movies {...item} index={index} key={item.shows.title} />
-        )
-        // console.log(index + " = " + item + " = " + item[index])
-        // console.log(item);
-        )}*/}
-            {/*{moviesList.map((item, index) => (
-        <Movies item={item} key={index} />
-      ))}*/}
+            {" "}
+            {/* Route for the home route */}
             <div className="header">
               <Header />
             </div>
             <h1>Popular Movies</h1>
             <div className="movie">
               <div className="movie-cards">
-                {moviesList.map((item, index) => (
-                  <Movies item={item} key={item.ids.trakt} />
-                ))}
+                {moviesList.map(
+                  (
+                    item,
+                    index //mapping the movies to the Movie component
+                  ) => (
+                    <Movies item={item} key={item.ids.trakt} />
+                  )
+                )}
               </div>
             </div>
             <div className="header">
               <Footer />
             </div>
           </Route>
+
           <Route exact path="/:id">
+            {" "}
+            {/* Route for the selected movie details route */}
             <div className="header">
               <Header />
             </div>
             <div className="cast">
-              <Casts />
+              <Casts /> {/* Cast component rendered here */}
             </div>
             <div className="header">
               <Footer />
--- frontend/src/components/casts/Casts.js
+++ frontend/src/components/casts/Casts.js
@@ -8,57 +8,50 @@ import Actors from "../actors/Actors";
 import "./casts.css";
 
 const Casts = () => {
-  const { id } = useParams();
+  const { id } = useParams(); //useParam to get the parameter which the :id on the url
 
   console.log(id);
 
-  // let dummy = id;
-
-  const [movieCast, setMovieCast] = useState([]);
-  // const [check, setCheck] = useState(null);
-  const [director, setDirector] = useState([]);
-  const [writer, setWriter] = useState([]);
-  const [actor, setActor] = useState([]);
+  const [movieCast, setMovieCast] = useState([]); //state for getting the whole cast array
+  const [director, setDirector] = useState([]); //state for getting the director array from the cast array
+  const [writer, setWriter] = useState([]); //state for getting the writer array from the cast array
+  const [actor, setActor] = useState([]); //state for getting the actor array from the cast array
 
   useEffect(() => {
     const castFunction = async () => {
-      // axios.get(`http://localhost:8080/`).then((response) => {
-      //   //   console.log(response.data);
-      //   setPopularMovies(response.data);
-      //   console.log(popularMovies);
-      //   // console.log(state);
-      //   // console.log("success");
-      // });
-
       try {
-        const res = await axios.get(`http://localhost:8080/cast/${id}`);
-        setMovieCast([res.data]);
-        setDirector(movieCast[0].directors.items);
-        setWriter(movieCast[0].writers.items);
-        setActor(movieCast[0].actors);
+        const res = await axios.get(`http://localhost:8080/cast/${id}`); //getting from the backend api
+        setMovieCast([res.data]); //setting up the whole cast array
+        setDirector(movieCast[0].directors.items); //setting up the director array
+        setWriter(movieCast[0].writers.items); //setting up the writer array
+        setActor(movieCast[0].actors); //setting up the actor array
       } catch (err) {}
     };
     castFunction();
-    // setCheck(id);
   }, []);
 
-  console.log(movieCast);
-  // console.log(director);
+  console.log(movieCast); //can use this console log or the one below to make changes and save to see the issue we talked on the meeting
+  console.log(director);
 
   return (
     <div className="container">
       <h2>Director</h2>
       <div className="director">
         <div className="director-cards"></div>
-        {director.map((item, index) => (
-          <Directors item={item} key={item.id} />
-        ))}
+        {director.map(
+          (
+            item,
+            index //mapping the director to the Director component
+          ) => (
+            <Directors item={item} key={item.id} />
+          )
+        )}
       </div>
       <h2>Writer</h2>
       <div className="writer">
         <div className="writer-cards">
           {writer.map((item, index) => (
-            <Writter item={item} key={item.id} />
+            <Writter item={item} key={item.id} /> //mapping the writer to the Writer component
           ))}
         </div>
       </div>
@@ -66,7 +59,7 @@ const Casts = () => {
       <div className="actors">
         <div className="actor-cards">
           {actor.map((item, index) => (
-            <Actors item={item} key={item.id} />
+            <Actors item={item} key={item.id} /> //mapping the Actor to the Actor component
           ))}
         </div>
       </div>
